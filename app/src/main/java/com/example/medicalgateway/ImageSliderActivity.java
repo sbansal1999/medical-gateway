7fac9a311de71c0877e24d3b84f5670d36158b25
Satyam <sbansal1999@gmail.com>
2020-11-06 11:58:58 +0530

    Enabled viewBinding in build.gradle and also adjusted accordingly in ImageSliderActivity.java

--- app/src/main/java/com/example/medicalgateway/ImageSliderActivity.java
+++ app/src/main/java/com/example/medicalgateway/ImageSliderActivity.java
@@ -2,53 +2,41 @@ package com.example.medicalgateway;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import java.util.Objects;
+import com.example.medicalgateway.databinding.ActivityImageSliderBinding;
 
 public class ImageSliderActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private ImageView image_dot_first, image_dot_second, image_dot_third;
     private final static int NUMBER_OF_IMAGES = 3;
-    private Button button_skip,button_next;
+    private ActivityImageSliderBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityImageSliderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         //Hide Action Bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        setContentView(R.layout.activity_image_slider);
-
-        viewPager = findViewById(R.id.viewPager_images);
-
-        image_dot_first = findViewById(R.id.image_dot_first);
-        image_dot_second = findViewById(R.id.image_dot_second);
-        image_dot_third = findViewById(R.id.image_dot_third);
-
-        button_skip = findViewById(R.id.button_skip);
-        button_next = findViewById(R.id.button_next);
-
         //Set Default Image Level
-        image_dot_first.setImageLevel(1);
-
+        binding.imageDotFirst.setImageLevel(1);
 
 
         SlidingImageAdapter imageAdapter = new SlidingImageAdapter(getSupportFragmentManager(),
                                                                    FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
                                                                    NUMBER_OF_IMAGES);
-        viewPager.setAdapter(imageAdapter);
+        binding.viewPagerImages.setAdapter(imageAdapter);
 
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        binding.viewPagerImages.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
                 highlightDot(position);
@@ -68,29 +56,29 @@ public class ImageSliderActivity extends AppCompatActivity {
 
     }
 
-
     /**
      * Method to change the dots below the viewPager according to the {@code position}
+     *
      * @param position the index of the image currently on the screen
      */
     private void highlightDot(int position) {
-        button_skip.setVisibility(View.VISIBLE);
-        button_next.setVisibility(View.INVISIBLE);
+        binding.buttonSkip.setVisibility(View.VISIBLE);
+        binding.buttonNext.setVisibility(View.INVISIBLE);
 
-        image_dot_first.setImageLevel(0);
-        image_dot_second.setImageLevel(0);
-        image_dot_third.setImageLevel(0);
+        binding.imageDotFirst.setImageLevel(0);
+        binding.imageDotSecond.setImageLevel(0);
+        binding.imageDotThird.setImageLevel(0);
         switch (position) {
             case 0:
-                image_dot_first.setImageLevel(1);
+                binding.imageDotFirst.setImageLevel(1);
                 break;
             case 1:
-                image_dot_second.setImageLevel(1);
+                binding.imageDotSecond.setImageLevel(1);
                 break;
             case 2:
-                image_dot_third.setImageLevel(1);
-                button_skip.setVisibility(View.INVISIBLE);
-                button_next.setVisibility(View.VISIBLE);
+                binding.imageDotThird.setImageLevel(1);
+                binding.buttonSkip.setVisibility(View.INVISIBLE);
+                binding.buttonNext.setVisibility(View.VISIBLE);
                 break;
         }
     }
