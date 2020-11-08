d988c25bd02ba6d3b7b2176b249f742c23009cfe
Satyam <sbansal1999@gmail.com>
2020-11-08 11:26:57 +0530

    Added ActivityRegisterBinding object "binding" in RegisterActivity.java and made some changes in activity_register.xml

--- app/src/main/java/com/example/medicalgateway/RegisterActivity.java
+++ app/src/main/java/com/example/medicalgateway/RegisterActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.medicalgateway.databinding.ActivityRegisterBinding;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
     }
 }
