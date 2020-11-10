99b9747c6392915cce518120be6bb0145c776317
Satyam <sbansal1999@gmail.com>
2020-11-10 15:45:44 +0530

    Added font file nunito_regular.ttf and changed accordingly in themes.xml, as well some positional changes in activity_main.xml

--- app/src/main/java/com/example/medicalgateway/MainActivity.java
+++ app/src/main/java/com/example/medicalgateway/MainActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.medicalgateway.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     //activity for the user to decide if he will login or register
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 
 }
