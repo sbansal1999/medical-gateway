1c869f9ee35948be0d11250d8bd2b0c021e81c3c
akhand769 <akhand769@gmail.com>
2020-11-11 21:35:38 +0530

    backcoding of register activity

--- app/src/main/java/com/example/medicalgateway/MainActivity.java
+++ app/src/main/java/com/example/medicalgateway/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.medicalgateway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.example.medicalgateway.databinding.ActivityMainBinding;
+import com.example.medicalgateway.databinding.ActivityRegisterBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     //activity for the user to decide if he will login or register
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.buttonRegisterMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+        binding.buttonLoginMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/medicalgateway/RegisterActivity.java
+++ app/src/main/java/com/example/medicalgateway/RegisterActivity.java
@@ -3,12 +3,59 @@ package com.example.medicalgateway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.medicalgateway.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private ActivityRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String password = binding.textPassword.getEditText().getText().toString();
+               String email = binding.textEmailAddress.getEditText().getText().toString();
+               String phone = binding.textNumber.getEditText().getText().toString();
+               String Password = binding.textPassword.getEditText().getText().toString();
+               String lwcase = ".*[a-z].*";
+               String upcase = ".*[A-Z].*";
+               String num = ".*[0-9].*";
+               String symbol = ".*[! @ # $ % ^ & *].*";
+               String regex = "^[a-zA-Z0-9._]+[0-9._]+@[a-zA-Z]+[.]+[a-z]+$";
+               String regexphone = "[0-9]{10}";
+               //val passwordlayout =
+               boolean checkpass = Password.matches(lwcase)&&Password.matches(upcase)&&Password.matches(num)&&Password.matches(symbol);
+               boolean check=email.matches(regex);
+               boolean checknum=phone.matches(regexphone);
+                if(!checkpass||Password.length()<6)
+                {
+                    binding.textPassword.setError("Password must be - \n" +
+                            "1 - Atleast 6 characters long \n" +
+                            "2 - Must contain a uppercase alphabet \n" +
+                            "3 - Must contain a lowercase alphabet \n" +
+                            "4 - Must contain a number \n" +
+                            "5 - Must contain a symbol - !,@,#,$,%,^,&,*");
+                }
+                if(checkpass)
+                {
+                    binding.textPassword.setError(null);
+                }
+               if(!checknum)
+               {
+                   Toast.makeText(RegisterActivity.this, "Invalid phone number", Toast.LENGTH_SHORT).show();
+               }
+               if(!check)
+               {
+                   Toast.makeText(RegisterActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
+               }
+
+            }
+        });
     }
 }
