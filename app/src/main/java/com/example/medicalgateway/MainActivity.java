de5c3c6134d65a29f5a28ab6972a10a2306f91a2
Satyam <sbansal1999@gmail.com>
2020-11-13 17:30:21 +0530

    Deleted some useless files and migrated OnClickListeners to onClick attribute in activity_register.xml and activity_main.xml :)

--- app/src/main/java/com/example/medicalgateway/MainActivity.java
+++ app/src/main/java/com/example/medicalgateway/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.medicalgateway;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.medicalgateway.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
     //activity for the user to decide if he will login or register
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +19,23 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
 
+    /**
+     * Takes the user to the Login Activity
+     * @param view Receives the button that was clicked
+     */
+    public void doLogin(View view) {
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
+    }
 
-        binding.buttonRegisterMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-        binding.buttonLoginMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
+    /**
+     * Takes the user to the Register Activity
+     * @param view Receives the button that was clicked
+     */
+    public void doRegister(View view) {
+        Intent myIntent = new Intent(this, RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/medicalgateway/RegisterActivity.java
+++ app/src/main/java/com/example/medicalgateway/RegisterActivity.java
@@ -1,69 +1,78 @@
 package com.example.medicalgateway;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.medicalgateway.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+
         View view = binding.getRoot();
         setContentView(view);
-        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               String password = binding.textPassword.getEditText().getText().toString();
-               String email = binding.textEmailAddress.getEditText().getText().toString();
-               String phone = binding.textNumber.getEditText().getText().toString();
-               String Password = binding.textPassword.getEditText().getText().toString();
-               String lwcase = ".*[a-z].*";
-               String upcase = ".*[A-Z].*";
-               String num = ".*[0-9].*";
-               String symbol = ".*[! @ # $ % ^ & *].*";
-               String regex = "^[a-zA-Z0-9._]+[0-9._]+@[a-zA-Z]+[.]+[a-z]+$";
-               String regexphone = "[0-9]{10}";
-               //val passwordlayout =
-               boolean checkpass = Password.matches(lwcase)&&Password.matches(upcase)&&Password.matches(num)&&Password.matches(symbol);
-               boolean check=email.matches(regex);
-               boolean checknum=phone.matches(regexphone);
-                if(!checkpass||Password.length()<6)
-                {
-                    binding.textPassword.setError("Password must be - \n" +
-                            "1 - Atleast 6 characters long \n" +
-                            "2 - Must contain a uppercase alphabet \n" +
-                            "3 - Must contain a lowercase alphabet \n" +
-                            "4 - Must contain a number \n" +
-                            "5 - Must contain a symbol - !,@,#,$,%,^,&,*");
-                }
-                if(checkpass)
-                {
-                    binding.textPassword.setError(null);
-                }
-               if(!checknum)
-               {
-                   binding.textNumber.setError("Invalid Number");
-               }
-                if(checknum)
-                {
-                    binding.textNumber.setError(null);
-                }
-               if(!check)
-               {
-                   binding.textEmailAddress.setError("Invalid Email");
-               }
-               if(check)
-               {
-                   binding.textEmailAddress.setError(null);
-               }
 
+    }
+
+    /**
+     * Method which performs the registration process
+     * @param view The Button that was clicked
+     */
+    public void doRegister(View view) {
+        {
+            String email = binding.textEmailAddress.getEditText().getText().toString();
+            String phone = binding.textNumber.getEditText().getText().toString();
+            String password = binding.textPassword.getEditText().getText().toString();
+            String regexLowerCase = ".*[a-z].*";
+            String regexUpperCase = ".*[A-Z].*";
+            String regexNum = ".*[0-9].*";
+            String regexSymbol = ".*[! @#$%^&*].*";
+            String regexEMail = "^[a-zA-Z0-9._]+[0-9._]+@[a-zA-Z]+[.]+[a-z]+$";
+            String regexPhone = "[0-9]{10}";
+
+
+            boolean checkPass = password.matches(regexLowerCase) && password.matches(regexUpperCase) && password
+                    .matches(regexNum) && password.matches(regexSymbol);
+            boolean checkEMail = email.matches(regexEMail);
+            boolean checkNum = phone.matches(regexPhone);
+
+
+            if (!checkPass || password.length() < 6) {
+                binding.textPassword.setError("Password must be - \n" +
+                                                      "1 - At Least 6 characters long \n" +
+                                                      "2 - Must contain a uppercase alphabet \n" +
+                                                      "3 - Must contain a lowercase alphabet \n" +
+                                                      "4 - Must contain a number \n" +
+                                                      "5 - Must contain a symbol - !,@,#,$,%,^,&,*");
             }
-        });
+
+            if (checkPass) {
+                binding.textPassword.setError(null);
+            }
+
+            if (!checkNum) {
+                binding.textNumber.setError("Invalid Number");
+            }
+
+            if (checkNum) {
+                binding.textNumber.setError(null);
+            }
+
+            if (!checkEMail) {
+                binding.textEmailAddress.setError("Invalid Email");
+            }
+
+            if (checkEMail) {
+                binding.textEmailAddress.setError(null);
+            }
+
+        }
     }
 }
