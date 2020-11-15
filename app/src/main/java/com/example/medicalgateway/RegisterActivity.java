3c70f9e955b5d52d6500eecc10440f654b386abc
akhand769 <akhand769@gmail.com>
2020-11-15 12:54:44 +0530

    fragments and portals added

--- app/src/main/java/com/example/medicalgateway/RegisterActivity.java
+++ app/src/main/java/com/example/medicalgateway/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.medicalgateway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -30,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
                String regex = "^[a-zA-Z0-9._]+[0-9._]+@[a-zA-Z]+[.]+[a-z]+$";
                String regexphone = "[0-9]{10}";
                //val passwordlayout =
+                int flag=0;
                boolean checkpass = Password.matches(lwcase)&&Password.matches(upcase)&&Password.matches(num)&&Password.matches(symbol);
                boolean check=email.matches(regex);
                boolean checknum=phone.matches(regexphone);
@@ -41,26 +43,46 @@ public class RegisterActivity extends AppCompatActivity {
                             "3 - Must contain a lowercase alphabet \n" +
                             "4 - Must contain a number \n" +
                             "5 - Must contain a symbol - !,@,#,$,%,^,&,*");
+                    flag=1;
                 }
                 if(checkpass)
                 {
                     binding.textPassword.setError(null);
+                    flag=0;
                 }
                if(!checknum)
                {
                    binding.textNumber.setError("Invalid Number");
+                   flag=1;
                }
                 if(checknum)
                 {
                     binding.textNumber.setError(null);
+                    flag=0;
                 }
                if(!check)
                {
                    binding.textEmailAddress.setError("Invalid Email");
+                   flag=1;
                }
                if(check)
                {
                    binding.textEmailAddress.setError(null);
+                   flag=0;
+               }
+               if(binding.checkBoxTc.isChecked()==false)
+               {
+                   Toast.makeText(RegisterActivity.this,"Please accept the terms and conditions",Toast.LENGTH_LONG).show();
+                   flag=1;
+               }
+               else
+               {
+                   flag=0;
+               }
+               if(flag==0)
+               {
+                   Intent myIntent = new Intent(RegisterActivity.this, PatientPortalActivity.class);
+                   RegisterActivity.this.startActivity(myIntent);
                }
 
             }
