7c0a9e9b36d7e819128c6146fa816a2c7c7f5010
akhand769 <akhand769@gmail.com>
2020-11-16 17:29:00 +0530

    code in ProfileFragment.java

--- app/src/main/java/com/example/medicalgateway/ProfileFragment.java
+++ app/src/main/java/com/example/medicalgateway/ProfileFragment.java
@@ -1,12 +1,19 @@
 package com.example.medicalgateway;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,8 +22,7 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -52,13 +58,21 @@ public class ProfileFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_profile_patient, null);
+
+
+        return root;
+
+
     }
 }
