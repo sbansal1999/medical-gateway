cf6417a2095d165ea0e59c3f7f5d2020ba114d34
Satyam Bansal <sbansal1999@gmail.com>
2020-11-17 16:28:16 +0530

    Added CropImage in ProfileFragment.java and some minor changes

--- app/src/main/java/com/example/medicalgateway/PatientPortalActivity.java
+++ app/src/main/java/com/example/medicalgateway/PatientPortalActivity.java
@@ -1,18 +1,20 @@
 package com.example.medicalgateway;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.medicalgateway.databinding.ActivityPatientportalBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PatientPortalActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-    private ActivityPatientportalBinding binding;
     private static final String TAG = "PatientPortalActivity";
+
+    private ActivityPatientportalBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +25,34 @@ public class PatientPortalActivity extends AppCompatActivity implements BottomNa
         binding.bottomNavigationPatientView.setSelectedItemId(R.id.navigation_home);
 
     }
-    HomeFragment homeFragment = new HomeFragment();
-    PharmacyPatientFragment pharmacyPatientFragment = new PharmacyPatientFragment();
-    ProfileFragment profileFragment = new ProfileFragment();
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId())
-        {
-            case R.id.navigation_home:
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in,R.anim.fade_out).replace(R.id.container , homeFragment).commit();
-                return true;
-            case R.id.navigation_pharmacy:
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in,R.anim.fade_out).replace(R.id.container , pharmacyPatientFragment).commit();
-                return true;
-            case R.id.navigation_profile:
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in,R.anim.fade_out).replace(R.id.container , profileFragment).commit();
-                return true;
+        int itemId = item.getItemId();
+        if (itemId == R.id.navigation_home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                    .replace(R.id.container, new HomeFragment())
+                    .commit();
+            return true;
+        } else if (itemId == R.id.navigation_pharmacy) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                    .replace(R.id.container, new PharmacyPatientFragment())
+                    .commit();
+            return true;
+        } else if (itemId == R.id.navigation_profile) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                    .replace(R.id.container, new ProfileFragment())
+                    .commit();
+            return true;
         }
         return false;
     }
+
+
 }
--- app/src/main/java/com/example/medicalgateway/ProfileFragment.java
+++ app/src/main/java/com/example/medicalgateway/ProfileFragment.java
@@ -3,76 +3,70 @@ package com.example.medicalgateway;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.medicalgateway.databinding.FragmentProfilePatientBinding;
 import com.theartofdev.edmodo.cropper.CropImage;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.NotNull;
+
+import static android.app.Activity.RESULT_OK;
+
+//TODO include apache license
+
 public class ProfileFragment extends Fragment {
 
+    private FragmentProfilePatientBinding binding;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfilePatientBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        binding.buttonUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
 
-    public ProfileFragment() {
-        // Required empty public constructor
+        return view;
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * Performs Uploading of the Image
      */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private void uploadImage() {
+        CropImage.activity()
+                 .start(getContext(), this);
+
+        //TODO add image picker
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode ,data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult activityResult = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = activityResult.getUri();
+                binding.circularImageView.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Toast.makeText(getContext(), "ERROR", Toast.LENGTH_SHORT)
+                     .show();
+            }
         }
-
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_profile_patient, null);
-
-
-        return root;
-
-
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
