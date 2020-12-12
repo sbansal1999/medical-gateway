1519af371aba81bd1ba37657fc7ef2bf1405d23f
Satyam Bansal <sbansal1999@gmail.com>
2020-12-12 11:59:01 +0530

    Revert "Akhand"

--- app/src/main/java/com/example/medicalgateway/ProfileFragment.java
+++ app/src/main/java/com/example/medicalgateway/ProfileFragment.java
@@ -1,20 +1,14 @@
 package com.example.medicalgateway;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.medicalgateway.databinding.FragmentProfilePatientBinding;
@@ -32,57 +26,14 @@ public class ProfileFragment extends Fragment {
     private static final int IMAGE_DIMEN = 1000;
     private FragmentProfilePatientBinding binding;
 
-    ArrayAdapter<CharSequence> adapter;
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfilePatientBinding.inflate(inflater, container, false);
-        adapter = ArrayAdapter.createFromResource(getContext(),R.array.blood_groups,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        binding.spinner.setAdapter(adapter);
-        binding.changeAddressButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                LayoutInflater li = LayoutInflater.from(getContext());
-                View promptsView = li.inflate(R.layout.prompts, null);
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        getContext());
-                alertDialogBuilder.setView(promptsView);
-
-                final EditText userInput = (EditText) promptsView
-                        .findViewById(R.id.editTextDialogUserInput);
-
-                // set dialog message
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("Change Address",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                        binding.textAddressPatient.setText(userInput.getText());
-                                    }
-                                })
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                // show it
-                alertDialog.show();
-
-
-            }
-        });
         binding.buttonUploadImage.setOnClickListener(v -> uploadImage());
 
         return binding.getRoot();
     }
-
 
     /**
      * Performs Uploading of the Image
