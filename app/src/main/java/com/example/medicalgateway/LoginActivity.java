1baa844395e46d16d1dd91325fd1001834190fb5
akhand769 <akhand769@gmail.com>
2020-12-14 05:37:50 +0530

    splash screen with net connectivity

--- app/src/main/java/com/example/medicalgateway/LoginActivity.java
+++ app/src/main/java/com/example/medicalgateway/LoginActivity.java
@@ -1,8 +1,25 @@
 package com.example.medicalgateway;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.net.NetworkInterface;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +27,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+
 }
--- app/src/main/java/com/example/medicalgateway/SplashActivity.java
+++ app/src/main/java/com/example/medicalgateway/SplashActivity.java
@@ -1,16 +1,78 @@
 package com.example.medicalgateway;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN =  3000;
 
     //TODO decide in this activity what screen is to be shown next
-
+Animation topanim,bottomanim;
+ImageView image;
+TextView title;
+Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        ConnectivityManager connectivitymanager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = connectivitymanager.getActiveNetworkInfo();
+
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomanim = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        image=findViewById(R.id.splash_image);
+        title = findViewById(R.id.textView2);
+        image.setAnimation(topanim);
+        title.setAnimation(bottomanim);
+        context = this;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(networkinfo==null || !networkinfo.isConnected() || !networkinfo.isAvailable())
+                {
+                    Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.alert_dialog);
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.getWindow().setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
+                            WindowManager.LayoutParams.WRAP_CONTENT);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.getWindow().getAttributes().windowAnimations =
+                            android.R.style.Animation_Dialog;
+                    Button btTryagain = dialog.findViewById(R.id.try_again_button);
+                    btTryagain.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            recreate();
+                        }
+                    });
+                    dialog.show();
+                }
+                else {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },SPLASH_SCREEN);
     }
 }
