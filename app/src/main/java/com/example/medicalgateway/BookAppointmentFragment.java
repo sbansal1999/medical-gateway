24740ae04815e5aa17994d422d3004df50374982
akhand769 <akhand769@gmail.com>
2021-03-26 10:58:56 +0530

    book appointment corrected

---
+++ app/src/main/java/com/example/medicalgateway/BookAppointmentFragment.java
@@ -0,0 +1,56 @@
+package com.example.medicalgateway;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.medicalgateway.databinding.FragmentBookappointmentBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class BookAppointmentFragment extends Fragment {
+
+    private static final int IMAGE_DIMEN = 1000;
+    private FragmentBookappointmentBinding binding;
+    private Calendar CurrentDate = Calendar.getInstance();
+
+
+    ArrayAdapter<CharSequence> DateAdapter;
+
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentBookappointmentBinding.inflate(inflater, container, false);
+        DateAdapter = ArrayAdapter.createFromResource(getContext(), R.array.Doctors, android.R.layout.simple_spinner_item);
+        DateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerDoctor.setAdapter(DateAdapter);
+        List<String> spinnerArray = new ArrayList<>();
+        CurrentDate.add(Calendar.DATE, 1);
+        String currentdate1 = DateFormat.getDateInstance(DateFormat.FULL).format(CurrentDate.getTime());
+        spinnerArray.add(currentdate1);
+        CurrentDate.add(Calendar.DATE, 1);
+        String currentdate2 = DateFormat.getDateInstance(DateFormat.FULL).format(CurrentDate.getTime());
+        spinnerArray.add(currentdate2);
+        CurrentDate.add(Calendar.DATE, 1);
+        String currentdate3 = DateFormat.getDateInstance(DateFormat.FULL).format(CurrentDate.getTime());
+        spinnerArray.add(currentdate3);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
+        DateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerDate.setAdapter(adapter2);
+        return binding.getRoot();
+    }
+
+
+
+
+
+}
