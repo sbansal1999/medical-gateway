3104c89881e3a1af42f376fcf2babffe8ded6f5d
akhand769 <akhand769@gmail.com>
2021-06-04 10:34:16 +0530

    search bar tried to add

--- app/src/main/java/com/example/medicalgateway/PharmacyAdapter.java
+++ app/src/main/java/com/example/medicalgateway/PharmacyAdapter.java
@@ -3,33 +3,35 @@ package com.example.medicalgateway;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class PharmacyAdapter extends RecyclerView.Adapter<PharmacyViewHolder>
-{
-ArrayList<PharmacyDataModel> data;
+public class PharmacyAdapter extends RecyclerView.Adapter<PharmacyViewHolder> implements Filterable {
+    ArrayList<PharmacyDataModel> data;
+    ArrayList<PharmacyDataModel> datacopy;
 
-    public PharmacyAdapter(ArrayList<PharmacyDataModel> data)
-    {
+    public PharmacyAdapter(ArrayList<PharmacyDataModel> data) {
         this.data = data;
+        datacopy = new ArrayList<>(data);
     }
 
     @NonNull
     @Override
     public PharmacyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
-        View view= inflater.inflate(R.layout.singlepharmacy_row,parent,false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.singlepharmacy_row, parent, false);
         return new PharmacyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PharmacyViewHolder holder, int position)
-    {
+    public void onBindViewHolder(@NonNull PharmacyViewHolder holder, int position) {
         holder.textMedName.setText(data.get(position).getMed_name());
         holder.textMedQty.setText(data.get(position).getQty_change());
         holder.textMedPrice.setText(data.get(position).getPrice_change());
@@ -42,4 +44,40 @@ ArrayList<PharmacyDataModel> data;
     public int getItemCount() {
         return data.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return dataFilter;
+    }
+
+    private Filter dataFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+ArrayList<PharmacyDataModel> filteredList= new ArrayList<>();
+if(charSequence == null || charSequence.length()==0)
+{
+    filteredList.addAll(datacopy);
+}else {
+    String filterPattern = charSequence.toString().toLowerCase().trim();
+    for(PharmacyDataModel item: datacopy)
+    {
+        if(item.getMed_name().toLowerCase().contains(filterPattern))
+        {
+            filteredList.add(item);
+        }
+    }
+}
+FilterResults results = new FilterResults();
+results.values = filteredList;
+return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+           data.clear();
+           data.addAll((ArrayList) filterResults.values);
+           notifyDataSetChanged();
+        }
+    };
+
 }
--- app/src/main/java/com/example/medicalgateway/PharmacyPatientFragment.java
+++ app/src/main/java/com/example/medicalgateway/PharmacyPatientFragment.java
@@ -2,9 +2,14 @@ package com.example.medicalgateway;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -22,6 +27,25 @@ public class PharmacyPatientFragment extends Fragment {
         Adapter=new PharmacyAdapter(dataqueue());
         binding.recylerviewPharmacy.setAdapter(Adapter);
         return binding.getRoot();
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_option,menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
     }
     public ArrayList<PharmacyDataModel> dataqueue()
     {
@@ -77,4 +101,6 @@ public class PharmacyPatientFragment extends Fragment {
 
         return holder;
     }
+
+
 }
