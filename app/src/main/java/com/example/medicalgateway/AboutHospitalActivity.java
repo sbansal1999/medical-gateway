1c49056b914f178e9613d3de91fd40d2c818f703
akhand769 <akhand769@gmail.com>
2021-06-29 11:09:15 +0530

    About hospital created

--- app/src/main/java/com/example/medicalgateway/AboutHospitalActivity.java
+++ app/src/main/java/com/example/medicalgateway/AboutHospitalActivity.java
@@ -1,14 +1,20 @@
 package com.example.medicalgateway;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class AboutHospitalActivity extends AppCompatActivity {
-
+    Toolbar toolbar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_hospital_activity);
+        toolbar1 = findViewById(R.id.include);
+        toolbar1.setTitle("About Hospital");
+        toolbar1.setTitleTextColor(Color.WHITE);
+        toolbar1.setTitleMarginStart(10);
     }
 }
